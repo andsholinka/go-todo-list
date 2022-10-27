8379dbe30d66149c504ecb300c7dc8e6972cdf50
Andrey Sholinka <sholinka2111@gmail.com>
2022-10-27 11:55:56 +0700

    first commit

---
+++ initializers/database.go
@@ -0,0 +1,29 @@
+package initializers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func CoonetToDB() {
+	var err error
+
+	dbUser := os.Getenv("MYSQL_USER")
+	dbPass := os.Getenv("MYSQL_PASSWORD")
+	dbHost := os.Getenv("MYSQL_HOST")
+	dbName := os.Getenv("MYSQL_DBNAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	// dsn := "root:@tcp(127.0.0.1:3306)/golang_gin?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connet to database")
+	}
+	// return DB
+}
