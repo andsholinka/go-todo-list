6a9e4f651ab633c4c64ce7964e818f11b669c74b
Andrey Sholinka <sholinka2111@gmail.com>
2022-10-27 22:47:15 +0700

    todoModel

--- models/todoModel.go
+++ models/todoModel.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Todo struct {
 	gorm.Model
-	ActivityGroupId string `json:"activity_group_id" form:"activity_group_id" binding:"required" validate:"required"`
-	Title           string `json:"title" form:"title" binding:"required" validate:"required"`
-	IsActive        bool   `json:"is_active" form:"is_active" binding:"required" validate:"required"`
-	Priority        string `json:"priority" form:"priority" binding:"required" validate:"required"`
+	ActivityGroupId string `json:"activity_group_id" form:"activity_group_id"`
+	Title           string `json:"title" form:"title"`
+	IsActive        bool   `json:"is_active" form:"is_active"`
+	Priority        string `json:"priority" form:"priority"`
 }
